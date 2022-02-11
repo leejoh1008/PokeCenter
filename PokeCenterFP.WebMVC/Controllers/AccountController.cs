03a9b4ef1657a2d31959262dc50c4cc129b0ee64
Johnson Lee <johnson.hwang1008@gmail.com>
2022-02-11 14:58:58 -0500

    e

--- PokeCenterFP.WebMVC/Controllers/AccountController.cs
+++ PokeCenterFP.WebMVC/Controllers/AccountController.cs
@@ -8,6 +8,7 @@ using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
+using PokeCenter.Data;
 using PokeCenterFP.WebMVC.Models;
 
 namespace PokeCenterFP.WebMVC.Controllers
