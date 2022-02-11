bc411909f41c808843a381eb047f172a9b42a8db
Johnson Lee <johnson.hwang1008@gmail.com>
2022-02-11 13:47:22 -0500

    first commit

---
+++ PokeCenterFP.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PokeCenterFP.WebMVC.Startup))]
+namespace PokeCenterFP.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
